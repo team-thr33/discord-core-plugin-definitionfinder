16a102929d9ced8e1a7a86441c8edfaf510be64f
Nick D <nicholasdavies192@gmail.com>
2019-03-23 11:42:26 +0000

    Initial Commit

---
+++ DiscordCore.Plugin.DefinitionFinder/DefinitionFinder.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using DiscordCore.Interfaces;
+using RestSharp;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace DiscordCore.Plugin.DefinitionFinder
+{
+    public class DefinitionFinder : IDiscordCorePlugin
+    {
+        private string _name = "UrbanDictionarySearcher";
+        private string _description = "Plugin that will find definitions for words from Urban Dictionary";
+        private IDiscordCoreEvents _discordEventClient;
+        private string _urbanDictionaryAPiURL = "http://api.urbandictionary.com/v0/define?term=";
+
+        public void Initalize(IDiscordCoreEvents discordEventClient)
+        {
+            _discordEventClient = discordEventClient;
+        }
+
+        private void _discordEventClient_newMessage(object sender, NewMessageEventArgs e)
+        {
+            if (e.NewMessage.Contains("@DefinitionFinder"))
+            {
+                AppDomain currentDomain = AppDomain.CurrentDomain;
+                List<Assembly> assems = currentDomain.GetAssemblies().ToList();
+                string searchString = _urbanDictionaryAPiURL + e.NewMessage.Replace("@DefinitionFinder", "");
+                var response = ApiCall(searchString);
+                string formattedResult = FormatResponse(response);
+                _discordEventClient.PostMessage(e.ChannelId, _name, formattedResult);
+            }
+        }
+
+        private UrbanDictionaryResponse ApiCall(string searchString)
+        {
+            var client = new RestClient(searchString);
+            var response = client.Execute<List<UrbanDictionaryResponse>>(new RestRequest());
+            var releases = response.Data;
+            return releases.FirstOrDefault();
+        }
+
+        private string FormatResponse(UrbanDictionaryResponse response)
+        {
+            string returnString = " ";
+            returnString += Environment.NewLine;
+            if (response.result_type != "no_results")
+            {
+                returnString += "**Meaning**: " + response.list.First().definition + Environment.NewLine;
+                returnString += "**Example**: " + response.list.First().example + Environment.NewLine;
+            }
+            else
+            {
+                returnString += "**Unable to find definition**";
+            }
+            return returnString;
+        }
+
+        public void Start()
+        {
+            _discordEventClient.NewMessage += _discordEventClient_newMessage;
+        }
+
+        public void Stop()
+        {
+            _discordEventClient.NewMessage -= _discordEventClient_newMessage;
+        }
+
+        public string GetPluginName()
+        {
+            return _name;
+        }
+
+        public string GetPluginDescription()
+        {
+            return _description;
+        }
+    }
+}
---
+++ DiscordCore.Plugin.DefinitionFinder/UrbanDictionaryResponse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace DiscordCore.Plugin.DefinitionFinder
+{
+    public class UrbanDictionaryResponseItem
+    {
+        public string definition { get; set; }
+        public string permalink { get; set; }
+        public int thumbs_up { get; set; }
+        public string author { get; set; }
+        public string word { get; set; }
+        public int defid { get; set; }
+        public string current_vote { get; set; }
+        public string example { get; set; }
+        public int thumbs_down { get; set; }
+    }
+
+    public class UrbanDictionaryResponse
+    {
+        public List<string> tags { get; set; }
+        public string result_type { get; set; }
+        public List<UrbanDictionaryResponseItem> list { get; set; }
+        public List<string> sounds { get; set; }
+    }
+}
